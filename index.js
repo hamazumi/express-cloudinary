4496fb3b08a044f32d455efcc215151e750b60aa
hamazumi <khamamoto12@gmail.com>
2021-06-08 10:38:59 -0700

    Finished Lesson

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var multer = require('multer')
+var upload = multer({ dest: './uploads/'})
+var cloudinary = require('cloudinary')
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -8,5 +12,13 @@ app.use(ejsLayouts);
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.post('/', upload.single('myFile'), (req, res) => {
+  cloudinary.uploader.upload(req.file.path, (result) => {
+    var imageId = `${result.public_id}.jpg`
+    var src = cloudinary.image(imageId, {effect: "grayscale", width: 224, crop: "fill"})
+    res.render('image', { imgSrc: src })
+  })
+})
 
 app.listen(3000);
